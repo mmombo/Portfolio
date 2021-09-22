49e4fc3530a71d6d4d040eac0f64f4332b23dc19
Matt Mombourquette <mmombou@uwo.ca>
2021-09-22 14:17:54 -0400

    first commit

---
+++ scripts/scrolldown.js
@@ -0,0 +1,11 @@
+const projectLink = document.querySelector("#project-link");
+const projects = document.querySelector("#projects");
+projectLink.onclick = function () {
+  projects.scrollIntoView({ behavior: "smooth", block: "start" });
+};
+
+const contactLink = document.querySelector("#contact-link");
+const contact = document.querySelector("#contact");
+contactLink.onclick = function () {
+  contact.scrollIntoView({ behavior: "smooth", block: "start" });
+};
